a8049f60a109aa76091a20bcece75bfcede110ca
poleinik <simpolly97@gmail.com>
2018-12-23 20:59:56 +0300

    models v2

--- models.py
+++ models.py
@@ -1,6 +1,16 @@
 
 from mongoengine import *
 
+class Houses(Document):
+    street=StringField(max_length=50)
+    number=IntField()
+    longtitude=FloatField()
+    latitude=FloatField()
+    meta={
+        'db_alias':'coord',
+        'collection':'Houses'
+    }
+
 class Streets(Document):
     name=StringField(max_length=50)
     longtitude=FloatField()
